b5008b90106aeeaa6878326b095c754fba99e698
Rajtilak <rajtilak@clavax.us>
2018-02-01 17:45:39 +0530

    set added

--- thenewsroom/thenewsroom/settings.py
+++ thenewsroom/thenewsroom/settings.py
@@ -138,5 +138,5 @@ MEDIA_URL = '/media/'
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATIC_ROOT = os.path.join(MEDIA_ROOT, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
