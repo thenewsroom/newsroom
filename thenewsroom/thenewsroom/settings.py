6e281fb91b95db90df8fbd99448671378acab72d
thenewsroom <35988385+thenewsroom@users.noreply.github.com>
2018-02-01 09:01:35 +0530

    added ip/domain in allowed hosts

--- thenewsroom/thenewsroom/settings.py
+++ thenewsroom/thenewsroom/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '9qeu%*gehg1ma_1g4x=op$hjcs36uef2xc-thnt_wiqt-s4q0t'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1',]
+ALLOWED_HOSTS = ['127.0.0.1', 'thenewsroom.co.in', '13.127.33.48']
 
 
 # Application definition
