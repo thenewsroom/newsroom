22f7aadcf266f02e6aeb5d3b91b43bbf22d642f8
Rajtilak <rajtilak@clavax.us>
2018-03-08 11:43:34 +0530

    preproduction merge

--- thenewsroom/content_management_system/admin.py
+++ thenewsroom/content_management_system/admin.py
@@ -5,13 +5,41 @@ from django.contrib import admin
 from .models import Content, ContentLoadStatus, Video
 from django import forms
 from .forms import ContentAdminForm
+from django.contrib.admin import SimpleListFilter
+from django.utils.translation import ugettext_lazy as _
 from datetime import datetime
 from tinymce.widgets import AdminTinyMCE
+
+
+class TrendingFilter(SimpleListFilter):
+    # Human-readable title which will be displayed in the
+    # right admin sidebar just above the filter options.
+    title = _('trending')
+
+    # Parameter for the filter that will be used in the URL query.
+    parameter_name = 'trending_filter'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('trending', _('trending')),
+            ('not_miss', _('not_miss')),
+            ('top_pick', _('top_pick'))
+        )
+
+    def queryset(self, request, queryset):
+        if self.value()== 'trending':
+            return queryset.filter(trending=True)
+        elif self.value()== 'not_miss':
+            return queryset.filter(not_miss=True)
+        elif self.value()== 'top_pick':
+            return queryset.filter(top_pick=True)
+        else:
+            return queryset
 
 class ContentAdmin(admin.ModelAdmin):
     form = ContentAdminForm
     list_display = ('title', 'publication', 'status',  'published_date',)
-    list_filter = ('created_on', 'status', 'category', 'subcategory', 'published_date', 'story_status')
+    list_filter = ('created_on', TrendingFilter, 'status', 'category', 'subcategory', 'published_date', 'story_status')
     search_fields = ['title', ]
     prepopulated_fields = {"slug": ('title',)}
     fieldsets = ((None, {
